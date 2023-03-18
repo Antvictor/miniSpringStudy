7bf1ed59c5f67c0f6dfe317eee903c7e29dd13f8
exccedY <17560726090@163.com>
2023-03-18 12:21:48 +0800

    refactor: remove ArgumentValues useless methods

--- src/main/java/com/minis/beans/ArgumentValues.java
+++ src/main/java/com/minis/beans/ArgumentValues.java
@@ -16,31 +16,14 @@ public class ArgumentValues {
     private void addIndexArgumentValue(Integer key, ArgumentValue argumentValue) {
         this.indexArgumentValues.put(key, argumentValue);
     }
-    public boolean hasIndexedArgumentValue(int index){
-        return this.indexArgumentValues.containsKey(index);
-    }
+
     public ArgumentValue getIndexedArgumentValue(int index) {
         return this.indexArgumentValues.get(index);
     }
     public void addGenericArgumentValue(Object value, String type) {
         this.genericArgumentValues.add(new ArgumentValue(value, type));
     }
-    public void addGenericArgumentValue(ArgumentValue newValue) {
-        if (null != newValue.getName()) {
-            // 循环判断是否有相同的名称，有就删除原来的值
-            this.genericArgumentValues.removeIf(currentValue -> currentValue.getName().equals(newValue.getName()));
-        }
-        this.genericArgumentValues.add(newValue);
-    }
-    public ArgumentValue getGenericArgumentValue(String requiredName) {
-        for (ArgumentValue value: genericArgumentValues) {
-            if (value.getValue() != null &&(null == requiredName || !requiredName.equals(value.getName()))) {
-                continue;
-            }
-            return value;
-        }
-        return null;
-    }
+
 
     public int getArgumentCount() { return this.genericArgumentValues.size(); }
     public boolean isEmpty() { return this.genericArgumentValues.isEmpty(); }
--- src/main/java/com/minis/test/impl/AsServiceImpl.java
+++ src/main/java/com/minis/test/impl/AsServiceImpl.java
@@ -7,8 +7,32 @@ import com.minis.test.AsService;
  * @date 2023/3/14
  **/
 public class AsServiceImpl implements AsService {
+    private String name;
+    private Integer age;
+
+    public AsServiceImpl(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
     @Override
     public void sayHello() {
         System.out.println("as service say Hello");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
     }
 }
